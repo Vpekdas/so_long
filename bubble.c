1e806b50df72489712da6e79a24bed9e27dacfbc
Inf1n1ty75 <volkandu75@gmail.com>
2024-02-26 20:02:11 +0100

    added some TODO to bubble

--- bubble.c
+++ bubble.c
@@ -6,7 +6,7 @@
 /*   By: vopekdas <vopekdas@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/26 18:36:46 by vopekdas          #+#    #+#             */
-/*   Updated: 2024/02/26 19:56:29 by vopekdas         ###   ########.fr       */
+/*   Updated: 2024/02/26 20:01:57 by vopekdas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,7 +35,9 @@ void	draw_anim_bubble(t_game *game, t_anim *anim, int x, int y)
 
 void	update_anim_bubble(t_game *game)
 {
-
+	// TODO: Create a linked list of bubble, generate the list of bubble by using rand() % 100
+	// TODO: If a bubble pos y is higher than 0, reset it to game->map.map_height
+	// TODO: Each bubble pos can also be generated with rand() > rand() % game->map.map_width for x and rand() % game->map.map_height for y
 	draw_anim_bubble(game, &game->anim_bubble, game->bubble.x, game->bubble.y - game->bubble.velocity_y + 10);
 	game->bubble.velocity_y += 0.08;
 }
